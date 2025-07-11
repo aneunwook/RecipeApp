52d1fa59279c5a3270bfc08dc031eb344a66f664
hyohee0613 <gygml9@gmail.com>
2025-07-11 17:50:27 +0900

    fix: LikeResponseDto 에러 수정

--- src/main/java/com/example/recipeapp/domain/like/controller/dto/LikeResponseDto.java
+++ src/main/java/com/example/recipeapp/domain/like/controller/dto/LikeResponseDto.java
@@ -2,9 +2,11 @@ package com.example.recipeapp.domain.like.controller.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 @AllArgsConstructor
 @Builder
+@Getter
 public class LikeResponseDto {
     private Long recipeId;
     private long likesCount;
